d292332937dc33889a946c93d3b2fb3391594291
dunnoconnor <michael.dunn.oconnor@gmail.com>
2022-05-24 12:53:01 -0400

    tutorial complete

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'estates-c59e8',
+    appId: '1:409655555694:web:be5ad814cbd0698a45587f',
+    databaseURL: 'https://estates-c59e8-default-rtdb.firebaseio.com',
+    storageBucket: 'estates-c59e8.appspot.com',
+    locationId: 'us-east1',
+    apiKey: 'AIzaSyDtxZ9_U0pAiHhqs_N8qR0xED7uY14-MPA',
+    authDomain: 'estates-c59e8.firebaseapp.com',
+    messagingSenderId: '409655555694',
+    measurementId: 'G-HBHHMHP8F8',
+  },
   production: false
 };
 
